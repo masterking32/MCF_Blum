8e890830ca05725db9dc15f0425a52192370d4d0
Amin.MasterkinG <lichwow_masterking@yahoo.com>
2024-12-04 15:00:22 +0330

    API updated to version: 1ceb2880ebea4f7f15786b431670d45b

---
+++ version_files/recent_files/api_files/Fgn8LQaC.js
@@ -0,0 +1,29 @@
+import {
+  d as n,
+  a as o,
+  c as s,
+  n as t,
+  j as a
+} from "./DgYQtzR3.js";
+const c = n({
+    __name: "Skeleton",
+    props: {
+      rounded: {
+        type: Boolean,
+        default: !1
+      }
+    },
+    setup(e) {
+      return (_, r) => (o(), s("div", {
+        class: t(["kit-skeleton", {
+          "is-rounded": e.rounded
+        }])
+      }, null, 2))
+    }
+  }),
+  d = a(c, [
+    ["__scopeId", "data-v-20511fcb"]
+  ]);
+export {
+  d as _
+};
