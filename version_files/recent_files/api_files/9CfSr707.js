efc7ebca6e52f88889b9b19383a6b0db783c03c8
Amin.MasterkinG <lichwow_masterking@yahoo.com>
2024-12-10 19:45:22 +0330

    API updated to version: 00cdf773b74e23447219e350879ff536

---
+++ version_files/recent_files/api_files/9CfSr707.js
@@ -0,0 +1,31 @@
+import {
+  _ as o
+} from "./BW3QS8JY.js";
+import {
+  j as t,
+  a as c,
+  c as r,
+  k as _
+} from "./4q655kMI.js";
+import "./Cw0SkN-f.js";
+import "./Cb1tY54G.js";
+import "./BnDIxUOZ.js";
+import "./SxfgLXAP.js";
+import "./BnJp0EaA.js";
+const a = {},
+  s = {
+    class: "welcome-page page"
+  };
+
+function n(m, p) {
+  const e = o;
+  return c(), r("div", s, [_(e)])
+}
+const k = t(a, [
+  ["render", n],
+  ["__scopeId", "data-v-1425555e"]
+]);
+export {
+  k as
+  default
+};
---
+++ version_files/recent_files/api_files/B7iK5Mn-.js
@@ -0,0 +1,38 @@
+import {
+  d as o,
+  o as n,
+  ab as s,
+  a as m,
+  m as c,
+  aS as p,
+  j as _
+} from "./4q655kMI.js";
+import {
+  u as r
+} from "./B8VumzBl.js";
+import "./gZ3aEVfU.js";
+import "./CrgZqG5E.js";
+const i = o({
+    __name: "memepad",
+    setup(d) {
+      const {
+        _flow: {
+          init: e,
+          destroy: a
+        }
+      } = r();
+      return e(), n(() => {
+        a()
+      }), s().memepad.memepadInitiation(), (f, l) => {
+        const t = p;
+        return m(), c(t)
+      }
+    }
+  }),
+  I = _(i, [
+    ["__scopeId", "data-v-6041c1d5"]
+  ]);
+export {
+  I as
+  default
+};
