d8d1d4b0d05faf333e82d53c0a154b6a9ff33a7e
Amin.MasterkinG <lichwow_masterking@yahoo.com>
2025-02-15 17:00:26 +0330

    API updated to version: af55f51b142b2f7c3a9a96a7a2080679

---
+++ version_files/recent_files/api_files/CocQy7Ky.js
@@ -0,0 +1,68 @@
+import {
+  _ as c
+} from "./D9cWcxd2.js";
+import {
+  _ as l
+} from "./CXAe61mn.js";
+import {
+  d,
+  o as t,
+  c as s,
+  G as n,
+  F as i,
+  a as o,
+  t as u,
+  q as y,
+  H as k,
+  bl as r,
+  D as f,
+  f as h
+} from "./CagiCBnQ.js";
+const v = {
+    class: "pages-tribe-top-list-template"
+  },
+  g = {
+    class: "heading"
+  },
+  b = {
+    class: "list"
+  },
+  B = {
+    class: "my-item-in-rank"
+  },
+  I = d({
+    __name: "TopListTemplate",
+    props: {
+      title: {},
+      items: {},
+      myItem: {}
+    },
+    setup(C) {
+      return (e, F) => {
+        const m = c,
+          _ = l;
+        return t(), s("div", v, [e.items ? (t(), s(i, {
+          key: 1
+        }, [o("div", g, u(e.title), 1), o("div", b, [(t(!0), s(i, null, y(e.items, (a, p) => r(e.$slots, "list-item", {
+          key: a.id,
+          item: a,
+          idx: p
+        }, void 0, !0)), 128))]), e.myItem ? (t(), n(_, {
+          key: 0
+        }, {
+          default: k(() => [o("div", B, [r(e.$slots, "my-item", {
+            item: e.myItem
+          }, void 0, !0)])]),
+          _: 3
+        })) : f("", !0)], 64)) : (t(), n(m, {
+          key: 0
+        }))])
+      }
+    }
+  }),
+  D = h(I, [
+    ["__scopeId", "data-v-a2ba3d31"]
+  ]);
+export {
+  D as _
+};
---
+++ version_files/recent_files/api_files/DD-HE6An.js
@@ -0,0 +1,78 @@
+import {
+  _ as b
+} from "./BvO1WobA.js";
+import {
+  _ as f
+} from "./CocQy7Ky.js";
+import {
+  d as k,
+  cE as v,
+  O as x,
+  Y as _,
+  ac as c,
+  o as g,
+  c as T,
+  b as o,
+  H as u,
+  e as s,
+  f as h
+} from "./CagiCBnQ.js";
+import "./BmGpysUj.js";
+import "./D9cWcxd2.js";
+import "./z_uMznOC.js";
+import "./CXAe61mn.js";
+const y = {
+    class: "pages-tribe-leaderboard page"
+  },
+  B = k({
+    __name: "leaderboard",
+    setup(R) {
+      const {
+        usersLeaderboard: r
+      } = v();
+      return x().initHandler(() => {
+        const e = _().previousRoute.value;
+        return () => c().push({
+          name: e ? e.name : "tribe-join"
+        })
+      }, "tribe-slug-leaderboard"), (() => {
+        var a;
+        const e = (a = _().previousRoute.value) == null ? void 0 : a.name;
+        c().replace({
+          name: e ?? "tribe-join"
+        })
+      })(), (e, a) => {
+        var i, m, p;
+        const n = b,
+          l = f;
+        return g(), T("div", y, [o(l, {
+          title: (i = s(r)) == null ? void 0 : i.title,
+          items: (m = s(r)) == null ? void 0 : m.items,
+          "my-item": (p = s(r)) == null ? void 0 : p.me
+        }, {
+          "list-item": u(({
+            item: t,
+            idx: d
+          }) => [o(n, {
+            user: t,
+            rank: d + 1
+          }, null, 8, ["user", "rank"])]),
+          "my-item": u(({
+            item: t
+          }) => [o(n, {
+            user: t,
+            rank: t.rank,
+            "no-open": ""
+          }, null, 8, ["user", "rank"])]),
+          _: 1
+        }, 8, ["title", "items", "my-item"])])
+      }
+    }
+  }),
+  P = h(B, [
+    ["__scopeId", "data-v-37b96330"]
+  ]);
+export {
+  P as
+  default
+};
