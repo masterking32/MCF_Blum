66791d5d51dd3a57159bff529e5a1cd8c7ef0fff
Amin.MasterkinG <lichwow_masterking@yahoo.com>
2025-02-17 15:40:24 +0330

    API updated to version: 423a1a891b7e31d48e881da60ea7ba8e

---
+++ version_files/recent_files/api_files/CURtSyud.js
@@ -0,0 +1,166 @@
+import {
+  d as V,
+  ao as x,
+  w as h,
+  bX as B,
+  o as n,
+  c as r,
+  f as E,
+  an as D,
+  K as i,
+  aF as F,
+  a8 as I,
+  T,
+  j as R,
+  ak as $,
+  N as q,
+  a as t,
+  F as z,
+  q as N,
+  n as w,
+  D as U,
+  t as A,
+  e as b,
+  bs as G,
+  bl as K
+} from "./DconZeyr.js";
+const W = V({
+    __name: "CheckboxLegal",
+    props: {
+      modelValue: {
+        type: Boolean,
+        required: !0
+      },
+      modelModifiers: {}
+    },
+    emits: ["update:modelValue"],
+    setup(c) {
+      const v = x(c, "modelValue");
+      return (a, o) => h((n(), r("input", {
+        "onUpdate:modelValue": o[0] || (o[0] = f => v.value = f),
+        type: "checkbox",
+        class: "checkbox-legal"
+      }, null, 512)), [
+        [B, v.value]
+      ])
+    }
+  }),
+  Z = E(W, [
+    ["__scopeId", "data-v-f5d0d034"]
+  ]),
+  j = {
+    class: "list"
+  },
+  H = ["data-tab"],
+  O = {
+    key: 0,
+    class: "ping-dot"
+  },
+  X = {
+    class: "label"
+  },
+  J = ["value", "name"],
+  k = .4,
+  P = V({
+    __name: "KitTabsInline",
+    props: D({
+      items: {
+        type: Array,
+        default: void 0
+      }
+    }, {
+      modelValue: {},
+      modelModifiers: {}
+    }),
+    emits: ["update:modelValue"],
+    setup(c) {
+      const v = c,
+        a = x(c, "modelValue"),
+        o = i(null),
+        f = () => {
+          var _;
+          const l = a.value,
+            e = (_ = o.value) == null ? void 0 : _.querySelector(`label[data-tab="${l}"]`);
+          if (e) {
+            const s = e.getBoundingClientRect(),
+              m = window.innerWidth - s.right,
+              S = s.left,
+              C = e.offsetLeft;
+            (m < 50 || S < 50) && (o.value.scrollLeft = C - window.innerWidth / 2 + s.width / 2)
+          }
+        };
+      setTimeout(() => f(), 10), F(a, f);
+      const y = i(!1),
+        g = i(!1),
+        d = i(null),
+        u = i(null),
+        p = new IntersectionObserver(([l]) => {
+          const e = l.intersectionRatio < k;
+          l.target.classList.contains("start") ? g.value = !e : y.value = !e
+        }, {
+          threshold: k
+        });
+      I(() => {
+        d.value && p.observe(d.value), u.value && p.observe(u.value)
+      }), T(() => {
+        d.value && p.unobserve(d.value), u.value && p.unobserve(u.value)
+      });
+      const L = R(() => {
+          var l;
+          return ((l = v.items) == null ? void 0 : l.map(e => typeof e == "string" ? {
+            value: e,
+            label: e
+          } : e)) ?? []
+        }),
+        M = $();
+      return (l, e) => {
+        const _ = q("horizontal-wheel-scroll");
+        return n(), r("div", {
+          class: w(["kit-tabs-inline", {
+            "is-fully-right-scrolled": b(y),
+            "is-fully-left-scrolled": b(g)
+          }])
+        }, [e[2] || (e[2] = t("div", {
+          class: "shadow left-shadow"
+        }, null, -1)), e[3] || (e[3] = t("div", {
+          class: "shadow right-shadow"
+        }, null, -1)), h((n(), r("div", {
+          ref_key: "tabsEl",
+          ref: o,
+          class: "content"
+        }, [t("div", j, [t("div", {
+          ref_key: "startEl",
+          ref: d,
+          class: "start"
+        }, null, 512), (n(!0), r(z, null, N(b(L), s => (n(), r("label", {
+          key: s.value,
+          class: w(["tab", {
+            "is-active": a.value === s.value,
+            "show-dot": s.dot
+          }]),
+          "data-tab": s.value
+        }, [s.pingDot ? (n(), r("div", O)) : U("", !0), t("span", X, A(s.label), 1), e[1] || (e[1] = t("div", {
+          class: "dot"
+        }, null, -1)), h(t("input", {
+          "onUpdate:modelValue": e[0] || (e[0] = m => a.value = m),
+          type: "radio",
+          value: s.value,
+          name: b(M)
+        }, null, 8, J), [
+          [G, a.value]
+        ])], 10, H))), 128)), t("div", {
+          ref_key: "endEl",
+          ref: u,
+          class: "end"
+        }, null, 512)]), K(l.$slots, "after", {}, void 0, !0)])), [
+          [_]
+        ])], 2)
+      }
+    }
+  }),
+  ee = E(P, [
+    ["__scopeId", "data-v-d98f083b"]
+  ]);
+export {
+  Z as _, ee as a
+};
---
+++ version_files/recent_files/api_files/D6XX6VCM.js
@@ -0,0 +1,194 @@
+import {
+  _ as T
+} from "./B4T-fyFT.js";
+import {
+  _ as k
+} from "./Dd8FeJIF.js";
+import {
+  d as u,
+  o as t,
+  c as a,
+  a as $,
+  t as f,
+  e as i,
+  z as h,
+  D as g,
+  f as _,
+  G as l,
+  H as j,
+  b as y,
+  _ as B,
+  K as I,
+  F as b,
+  p as x,
+  q as N,
+  aG as P,
+  cN as V,
+  bS as q,
+  O as R,
+  ac as S
+} from "./DconZeyr.js";
+import {
+  _ as K
+} from "./DgzOynJ8.js";
+import {
+  _ as C
+} from "./QlkasoEw.js";
+import "./DKELU81l.js";
+import "./BW2U6KP1.js";
+import "./DFaJ_cH9.js";
+import "./BPsI30yc.js";
+const F = {
+    class: "pages-tribe-search-no-results"
+  },
+  L = {
+    class: "title"
+  },
+  O = {
+    key: 0,
+    class: "subtitle"
+  },
+  w = u({
+    __name: "TribeSearchNoResults",
+    props: {
+      noSubtitle: {
+        type: Boolean,
+        default: !1
+      }
+    },
+    setup(e) {
+      return (n, s) => (t(), a("div", F, [$("div", L, f(("t" in n ? n.t : i(h))("tribes.search.no_results.title")), 1), e.noSubtitle ? g("", !0) : (t(), a("div", O, f(("t" in n ? n.t : i(h))("tribes.search.no_results.subtitle")), 1))]))
+    }
+  }),
+  A = _(w, [
+    ["__scopeId", "data-v-7b7a9484"]
+  ]),
+  D = u({
+    __name: "TribeSearchItem",
+    props: {
+      tribe: {
+        type: Object,
+        required: !0
+      },
+      source: {
+        type: String,
+        required: !0
+      }
+    },
+    setup(e) {
+      return (n, s) => {
+        const o = B,
+          r = K;
+        return t(), l(r, {
+          tribe: e.tribe,
+          source: e.source,
+          class: "pages-tribe-search-item"
+        }, {
+          default: j(() => [y(o, {
+            name: "chevron-right",
+            class: "icon"
+          })]),
+          _: 1
+        }, 8, ["tribe", "source"])
+      }
+    }
+  }),
+  E = _(D, [
+    ["__scopeId", "data-v-3c0fb88f"]
+  ]),
+  G = {
+    class: "pages-tribe-search"
+  },
+  H = {
+    key: 1,
+    class: "list"
+  },
+  U = u({
+    __name: "TribeSearch",
+    props: {
+      list: {
+        type: Array,
+        default: void 0
+      },
+      isSearching: {
+        type: Boolean,
+        required: !0
+      },
+      initialSearch: {
+        type: String,
+        default: void 0
+      },
+      joinTribeSource: {
+        type: String,
+        required: !0
+      }
+    },
+    emits: ["search"],
+    setup(e) {
+      const s = I(e.initialSearch ?? "");
+      return (o, r) => {
+        const m = T,
+          p = k,
+          d = A,
+          v = E;
+        return t(), a("div", G, [!e.list && !e.isSearching ? (t(), l(m, {
+          key: 0
+        })) : (t(), a(b, {
+          key: 1
+        }, [y(p, {
+          modelValue: i(s),
+          "onUpdate:modelValue": r[0] || (r[0] = c => x(s) ? s.value = c : null),
+          title: ("t" in o ? o.t : i(h))("tribes.search.title"),
+          "is-loading": e.isSearching,
+          onApply: r[1] || (r[1] = c => o.$emit("search", c))
+        }, null, 8, ["modelValue", "title", "is-loading"]), e.list ? (t(), a(b, {
+          key: 0
+        }, [e.list.length === 0 ? (t(), l(d, {
+          key: 0,
+          class: "no-results"
+        })) : (t(), a("div", H, [(t(!0), a(b, null, N(e.list, c => (t(), l(v, {
+          key: c.id,
+          tribe: c,
+          source: e.joinTribeSource
+        }, null, 8, ["tribe", "source"]))), 128))]))], 64)) : g("", !0)], 64))])
+      }
+    }
+  }),
+  z = _(U, [
+    ["__scopeId", "data-v-161e69bb"]
+  ]),
+  M = {
+    class: "tribe-join-list page"
+  },
+  W = u({
+    __name: "list",
+    setup(e) {
+      const n = P(),
+        s = V();
+      return q(() => n.tribeOrUndefined.value && S().replace({
+        name: "tribe-slug"
+      })), R().initHandler(() => S().push({
+        name: "tribe-join"
+      }), "tribe-join-list"), (o, r) => {
+        const m = z,
+          p = C;
+        return t(), a("div", M, [i(n).myTribeRes.value ? (t(), l(m, {
+          key: 0,
+          list: i(s).list.value,
+          "initial-search": i(s).searchLine.value,
+          "is-searching": i(s).isSearching.value,
+          "join-tribe-source": i(s).joinTribeSource.value,
+          onSearch: r[0] || (r[0] = d => i(s).search(d))
+        }, null, 8, ["list", "initial-search", "is-searching", "join-tribe-source"])) : (t(), l(p, {
+          key: 1
+        }))])
+      }
+    }
+  }),
+  ne = _(W, [
+    ["__scopeId", "data-v-5ccf0b2c"]
+  ]);
+export {
+  ne as
+  default
+};
